d0347c4f32f07dfb7ad4eba36873ba64271afbd8
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-08-08 20:05:39 +0530

    few changes

--- src/components/cart/CartItemCard.tsx
+++ src/components/cart/CartItemCard.tsx
@@ -1,50 +1,51 @@
 import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
-import {BsBookmarkHeart} from "react-icons/bs";
+import {BiHeart} from "react-icons/bi";
+import {FaRegTrashAlt} from "react-icons/fa";
 
 const CartItemCard = () => {
 
     return (
-        <div className='w-full p-6'>
-            <div className='flex justify-between items-center'>
+        <div className='w-full p-6 shadow-xl rounded-3xl mb-4'>
+            <div className='flex justify-between'>
                 <div className='flex gap-x-6'>
                     <div className='bg-black/[0.075] rounded-md flex items-center h-16 w-16 md:h-28 md:w-28'>
                         <img src='https://eyesome.netlify.app/static/media/sports1.f986df729a29a685f835.png'
                              alt="image" className='object-fit w-full'/>
                     </div>
                     <div>
-                        <h3 className='text-xl py-3 font-semibold'>Hyperclax Active</h3>
-                        <div className='flex gap-2 items-center'>
-                            <span className='text-gray-700'>Quantity: </span>
-                            <button
-                                className='bg-neutral p-1 text-gray-100 rounded-md  text-xs disabled:cursor-not-allowed'>
-                                <AiOutlineMinus/>
-                            </button>
-                            <span
-                                className='h-full w-10 bg-black/[0.075]  rounded-sm flex items-center justify-center'>
-                        1
-                    </span>
-                            <button
-                                className='bg-neutral p-1 text-gray-100 rounded-md text-xs disabled:cursor-not-allowed'>
-                                <AiOutlinePlus/>
-                            </button>
-                        </div>
-                        <div className='flex gap-1 sm:gap-3'>
-                            <button
-                                className='btn-rounded-secondary  text-xs sm:text-sm mt-2 max-w-xs disabled:cursor-not-allowed'>
-                                Remove from Bag
-                            </button>
-                            <button className='disabled:cursor-not-allowed'>
-                                <BsBookmarkHeart
-                                    className='text-xl hover:text-rose-600 hover:shadow-md transition'/>
-                            </button>
+                        <h3 className='lg:text-xl py-3 font-semibold'>Hyperclax Active</h3>
+                        <div>
+                            <span className='text-gray-700 mb-6'>Quantity: </span>
+                            <div className='flex my-3'>
+                                <button
+                                    className='btn btn-neutral btn-xs disabled:cursor-not-allowed'>
+                                    <AiOutlineMinus/>
+                                </button>
+                                <span
+                                    className='h-full w-10 bg-black/[0.075]  rounded-sm flex items-center justify-center'>
+                                    1
+                                </span>
+                                <button
+                                    className='btn btn-neutral btn-xs rounded-md text-xs disabled:cursor-not-allowed'>
+                                    <AiOutlinePlus/>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col">
-                    <span>Rs. 5000</span>
-                    <span className="text-xs line-through text-gray-600">
-                        Rs. 6500
-                    </span>
+                <div className="flex justify-around flex-col">
+                    <div className='flex sm:gap-3 mt-2 self-end'>
+                        <button className='btn btn-ghost btn-circle cursor-pointer disabled:cursor-not-allowed'>
+                            <BiHeart size={24} className='text-primary'/>
+                        </button>
+                        <button className='btn btn-ghost btn-circle cursor-pointer disabled:cursor-not-allowed'>
+                            <FaRegTrashAlt size={20} className='text-primary'/>
+                        </button>
+                    </div>
+                    <div className='self-end'>
+                        <span className='text-lg md:text-2xl mr-2'>Rs.5000</span>
+                        <span className='text-xs line-through text-gray-600 hidden md:inline'>Rs.6500</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -6,7 +6,7 @@ const Cart = () => {
     return (
         <div className='px-6 md:px-24'>
             <h2 className='text-2xl font-bold my-6'>Cart (2)</h2>
-            <div className='md:grid md:grid-cols-3 gap-5'>
+            <div className='md:grid md:grid-cols-3 gap-5 my-8'>
                 <div className='md:col-span-2'>
                     <CartItemCard/>
                     <CartItemCard/>
